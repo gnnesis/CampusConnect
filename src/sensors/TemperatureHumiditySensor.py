1ccdf2d1da559b672c799cafd5125fbe831c524b
Hugo Rey Insausti <hugo.rey@opendeusto.es>
2025-11-27 16:27:57 +0100

    Error fixed in TemeperatureHumiditySensor Co-authored-by: Genesis Balcazar <g.balcazar@opendeusto.es>

--- src/sensors/TemperatureHumiditySensor.py
+++ src/sensors/TemperatureHumiditySensor.py
@@ -1,34 +1,7 @@
-from grove.dht import DHT
+from seeed_dht import DHT
 
-th = DHT("11", 26)
+th = DHT('11', 26)
 
 def readTemperatureHumidity():
 	hum, temp = th.read()
 	return temp, hum
-/home/pi/grove.py/grove/grove_temperature_humidity_sensor_sht3x.py
-/home/pi/grove.py/grove/grove_temperature_humidity_bme680.py
-/home/pi/grove.py/grove/grove_temperature_sensor.py
-/home/pi/grove.py/grove/grove_high_accuracy_temperature.py
-/home/pi/grove.py/grove/temperature
-/home/pi/grove.py/grove/grove_temperature_humidity_aht20.py
-/home/pi/grove.py/CampusConnect/lib/python3.13/site-packages/grove/grove_temperature_humidity_sensor_sht3x.py
-/home/pi/grove.py/CampusConnect/lib/python3.13/site-packages/grove/__pycache__/grove_temperature_humidity_sensor_sht3x.cpython-313.pyc
-/home/pi/grove.py/CampusConnect/lib/python3.13/site-packages/grove/__pycache__/grove_temperature_humidity_bme680.cpython-313.pyc
-/home/pi/grove.py/CampusConnect/lib/python3.13/site-packages/grove/__pycache__/grove_temperature_humidity_aht20.cpython-313.pyc
-/home/pi/grove.py/CampusConnect/lib/python3.13/site-packages/grove/__pycache__/grove_high_accuracy_temperature.cpython-313.pyc
-/home/pi/grove.py/CampusConnect/lib/python3.13/site-packages/grove/__pycache__/grove_temperature_sensor.cpython-313.pyc
-/home/pi/grove.py/CampusConnect/lib/python3.13/site-packages/grove/grove_temperature_humidity_bme680.py
-/home/pi/grove.py/CampusConnect/lib/python3.13/site-packages/grove/grove_temperature_sensor.py
-/home/pi/grove.py/CampusConnect/lib/python3.13/site-packages/grove/grove_high_accuracy_temperature.py
-/home/pi/grove.py/CampusConnect/lib/python3.13/site-packages/grove/temperature
-/home/pi/grove.py/CampusConnect/lib/python3.13/site-packages/grove/grove_temperature_humidity_aht20.py
-/home/pi/grove.py/CampusConnect/bin/grove_temperature_humidity_sht31
-/home/pi/grove.py/CampusConnect/bin/grove_high_accuracy_temperature
-/home/pi/grove.py/CampusConnect/bin/grove_temperature_sensor
-/home/pi/grove.py/CampusConnect/bin/grove_temperature_humidity_bme680
-/home/pi/grove.py/build/lib/grove/grove_temperature_humidity_sensor_sht3x.py
-/home/pi/grove.py/build/lib/grove/grove_temperature_humidity_bme680.py
-/home/pi/grove.py/build/lib/grove/grove_temperature_sensor.py
-/home/pi/grove.py/build/lib/grove/grove_high_accuracy_temperature.py
-/home/pi/grove.py/build/lib/grove/temperature
-/home/pi/grove.py/build/lib/grove/grove_temperature_humidity_aht20.py
