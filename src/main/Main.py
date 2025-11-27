102e81cb18fd674ae4c34541b89056ff52d114e1
Hugo Rey Insausti <hugo.rey@opendeusto.es>
2025-11-27 15:48:10 +0100

    Fixed Main.py not reading other classes error Co-authored-by: Genesis Balcazar Escobar <g.balcazar@opendeusto.es

--- src/main/Main.py
+++ src/main/Main.py
@@ -1,18 +1,26 @@
 import time
+import sys
+import os 
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'sensors'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'display'))
+
 from TemperatureHumiditySensor import readTemperatureHumidity
 from NoiseSensor import readNoise
 from AirQualitySensor import readAirQuality
 from UltrasonicSensor import arePeople
+from LedBarActuator import showStatus
+from LcdDisplay import updateLcd
 
 while True:
 	temp, hum = readTemperatureHumidity()
 	noise = readNoise()
 	airQuality = readAirQuality()
-	people = arePeople
+	people = arePeople()
 
 	if airQuality == "Bad" or noise == "High" or people:
 		showStatus("Red")
-	elif airQuality == "Medium" or air == "Regular":
+	elif airQuality == "Medium" or noise == "Regular":
 		showStatus("Yellow")
 	else:
 		showStatus("Green")
