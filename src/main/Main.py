ca35cbba3690645d0f815979ec2f86c8f0dbe6a2
Hugo Rey Insausti <hugo.rey@opendeusto.es>
2025-12-04 12:40:48 +0100

    Led bar half-working

--- src/main/Main.py
+++ src/main/Main.py
@@ -7,24 +7,21 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'display'))
 
 from TemperatureHumiditySensor import readTemperatureHumidity
 from NoiseSensor import noiseLevel
+from NoiseSensor import readNoise
 from AirQualitySensor import airQuality
 from UltrasonicSensor import arePeople
-from LedBarActuator import showStatus
+from LedBarActuator import showNoiseLevel
 from LcdDisplay import updateLcd
 
 while True:
 	temp, hum = readTemperatureHumidity()
-	noise = noiseLevel()
+	noiseValue = readNoise()
+	noiseStatus = noiseLevel()
 	airStatus = airQuality()
 	people = arePeople()
 
-	if airStatus == "Bad" or noise == "High" or people:
-		showStatus("Red")
-	elif airStatus == "Medium" or noise == "Regular":
-		showStatus("Yellow")
-	else:
-		showStatus("Green")
+	showNoiseLevel(noiseStatus)
 
-	updateLcd(f"T: {temp:.1f}C", f"H: {hum:.1f}%")
+	updateLcd(f"T: {temp:.1f}C N: {noiseValue}", f"H: {hum:.1f}%")
 
 	time.sleep(1)
