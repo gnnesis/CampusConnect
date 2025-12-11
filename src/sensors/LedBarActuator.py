3f9270495bb4b8bf75cd724582bb20e6554f1491
HugoReyIn <hugo.rey@opendeusto.es>
2025-12-11 16:30:56 +0100

    ledbar v9
    Co-Authored-By: Génesis Balcazar <146766118+gnnesis@users.noreply.github.com>

--- src/sensors/LedBarActuator.py
+++ src/sensors/LedBarActuator.py
@@ -2,13 +2,6 @@ import RPi.GPIO as GPIO
 import time
 
 
-for i in range(10):
-    print("Encendiendo solo el LED", i+1)
-    for n in range(10):
-        ledBar.send_16bit(0xFFFF if n == i else 0x0000)
-    ledBar.latch_data()
-    time.sleep(1)
-
 class MY9221:
     def __init__(self, data_pin, clock_pin):
         self.data_pin = data_pin
@@ -51,6 +44,13 @@ class MY9221:
            Medium: LEDs 2-10
            High:   LEDs 1-10
         """
+
+        for i in range(10):
+            print("Encendiendo solo el LED", i+1)
+            for n in range(10):
+                ledBar.send_16bit(0xFFFF if n == i else 0x0000)
+            ledBar.latch_data()
+            time.sleep(1)
 
         if state == self.current_state:
             return
